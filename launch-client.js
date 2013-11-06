a28bc01d1309f16d4fd9930f3e752d9bf9c5f4c1
Jarrod Whelan <jrrd79@gmail.com>
2013-11-06 01:19:48 -0800

    adding client and server launchers to the root directory

---
+++ launch-client.js
@@ -0,0 +1,9 @@
+
+console.log('');
+console.log('Welcome to the Music Queue client.');
+console.log('In the future we will offer you some info here on how to add music to the shared queue.');
+console.log('');
+
+
+// kick off client app
+require('./lib/client/ClientApp');
---
+++ launch-server.js
@@ -0,0 +1,9 @@
+
+/*
+
+TODO:
+
+Here we will have to start a queue and create a LocalQueuePlayer to read the queue and 
+kick off requests to connected clients
+
+*/
