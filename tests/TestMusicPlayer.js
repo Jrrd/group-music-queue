8fdd70dd446546e2faa38f95175c71bf4d87f42b
Jarrod Whelan <jrrd79@gmail.com>
2013-11-06 01:07:29 -0800

    added ability to stream mp3 from client to server
    allow the server to accept multiple stream types

--- tests/TestMusicPlayer.js
+++ tests/TestMusicPlayer.js
@@ -1,22 +1,41 @@
-var MusicPlayer = require('../lib/server/MusicPlayer.js');
+var http = require('http'),
+	MusicPlayer = require('../lib/server/MusicPlayer.js');
 
 var mp = new MusicPlayer();
 
-mp.playFile("./sample.mp3");
-
-setTimeout(function() {
-	console.log('timeout');
-	// pause after 4 secs
-	mp.pause();
+function testPlayFile() {
+	
+	mp.playFile("./sample.mp3");
 	
 	setTimeout(function() {
-		console.log('resume');
+		console.log('timeout');
+		// pause after 4 secs
+		mp.pause();
 		
-		// resume after another 4 secs
-		mp.resume();
+		setTimeout(function() {
+			console.log('resume');
+			
+			// resume after another 4 secs
+			mp.resume();
+			
+		}, 5000);
 		
-	}, 5000);
-	
-	
-}, 2000);
+		
+	}, 2000);
 
+}
+// testPlayFile();
+
+function testPlayHttpStream() {
+	http.get("http://localhost:2500/musicstream", function(res) {
+		console.log("Got response: " + res.statusCode);
+		
+		mp.playHttpStream(res);
+		
+	})
+	.on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+}
+
+testPlayHttpStream();
