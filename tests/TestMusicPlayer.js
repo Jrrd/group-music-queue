98ad9425ebb64b58324519d35d3307952b252285
Jarrod Whelan <jrrd79@gmail.com>
2013-11-08 01:49:02 -0800

    simplified JSON parsing from request body + added client heartbeat endpoint
    To get express.bodyParser to work properly I had to set the
    Content-Type header appropriately.

--- tests/TestMusicPlayer.js
+++ tests/TestMusicPlayer.js
@@ -4,71 +4,108 @@ var http = require('http'),
 
 var mp = new MusicPlayer();
 
-function testPlayFile() {
-	
-	mp.playFile("./sample.mp3");
-	
-	setTimeout(function() {
-		console.log('timeout');
-		// pause after 4 secs
-		mp.pause();
+module.exports = {
+			
+	testPlayFile : function () {
+		
+		mp.playFile("./sample.mp3");
 		
 		setTimeout(function() {
-			console.log('resume');
+			console.log('timeout');
+			// pause after 4 secs
+			mp.pause();
 			
-			// resume after another 4 secs
-			mp.resume();
+			setTimeout(function() {
+				console.log('resume');
+				
+				// resume after another 4 secs
+				mp.resume();
+				
+			}, 5000);
 			
-		}, 5000);
-		
-		
-	}, 2000);
-
-}
-// testPlayFile();
-
-function testPlayHttpStream() {
-	http.get("http://localhost:2500/musicstream", function(res) {
-		console.log("Got response: " + res.statusCode);
-		
-		mp.playHttpStream(res);
-		
-	})
-	.on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
-}
-// testPlayHttpStream();
-
-function testAddingToQueue() {
+			
+		}, 2000);
 	
-	var postData = querystring.stringify({
-			client : 'localhost',
-			port : 2500,
-			filepath : '/Users/jarrod/Music/iTunes/iTunes Music/AC_DC/Back In Black/01 Hells Bells.mp3'
-			// filepath : '/git-personal/group-music-queue/tests/sample.mp3'
-		}),
-		options = {
-			host : 'localhost',
-			port : '3800',
-			method : 'POST',
-			path : '/add',
-			headers: {
-				// 'Content-Type': 'application/x-www-form-urlencoded',
-				// 'Content-Length': postData.length
+	},
+	// testPlayFile();
+	
+	testPlayHttpStream : function() {
+		http.get("http://localhost:2500/musicstream", function(res) {
+			console.log("Got response: " + res.statusCode);
+			
+			mp.playHttpStream(res);
+			
+		})
+		.on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
+	},
+	// testPlayHttpStream();
+	
+	testAddingToQueue : function() {
+		
+		var options = {
+				host : 'localhost',
+				port : '3800',
+				method : 'POST',
+				path : '/add',
+				headers: {
+					'Content-Type': 'application/json',
+					// 'Content-Length': postData.length
+				}
 			}
-		};
+		,	postData = JSON.stringify({
+				client : 'localhost',
+				port : 2500,
+				filepath : '/Users/jarrod/Music/iTunes/iTunes Music/AC_DC/Back In Black/01 Hells Bells.mp3'
+				// filepath : '/git-personal/group-music-queue/tests/sample.mp3'
+			})
+		;
+		
+		var req = http.request(options, function(res) {
+			if (res.statusCode === 204) {
+				console.log('Successful addition to the queue');
+			}
+		}).on('error', function() {
+			console.log('Bad Request.');
+		});
+		
+		console.log('sending: ' + postData);
+		req.end(postData);
+		
+	},
+	// testAddingToQueue();
 	
-	var req = http.request(options, function(res) {
-		if (res.statusCode === 204) {
-			console.log('Successful addition to the queue');
-		}
-	}).on('error', function() {
-		console.log('Bad Request.');
-	});
+	testHitClientHeartbeat : function() {
+		var postData = {
+				sentFrom: "test-client",
+				time: new Date()
+			},
+		
+			options = {
+				host : 'localhost',
+				port : '2500',
+				method : 'POST',
+				path : '/heartbeat',
+				headers: {
+					'Content-Type': 'application/json'
+					//, 'Content-Length': postData.length
+				}
+			};
 	
-	console.log('sending: ' + postData);
-	req.end(postData);
-	
-}
-testAddingToQueue();
+		var req = http.request(options, function(res) {
+			if (res.statusCode === 204) {
+				console.log('Successful addition to the queue');
+			}
+		}).on('error', function() {
+			console.log('Bad Request.');
+		});
+		
+		console.log('sending: ', postData);
+		
+		req.end(JSON.stringify(postData));
+		
+	}
+	// testHitClientHeartbeat();
+
+};
