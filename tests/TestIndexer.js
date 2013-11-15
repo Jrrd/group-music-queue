aebe4be70c1ed8a15f61b0a2669c2a0cc1be4e79
Frank Hanner <luapdeux@gmail.com>
2013-11-14 20:58:34 -0800

    Finished initial version of IndexPool. Started adding some index pool testing.

--- tests/TestIndexer.js
+++ tests/TestIndexer.js
@@ -1,5 +1,6 @@
 var Indexer = require('../lib/shared/utils/Indexer'),
-	http = require('http');
+	http = require('http'),
+	fs = require('fs');
 
 var indexer = new Indexer("frank");
 
@@ -14,9 +15,30 @@ module.exports = {
 	},
 	
 	indexFileSendToServerTest : function() {
+		var postData = fs.readFileSync("./song-index.json", 'utf8');
+		var options = {
+				host : 'localhost',
+				port : '3800',
+				method : 'POST',
+				path : '/indexfile',
+				headers: {
+					'Content-Type': 'application/json',
+					'Content-Length': Buffer.byteLength(postData)				
+				}
+			};
 		
+		var req = http.request(options, function(res) {
+			if (res.statusCode === 204) {
+				console.log('Successful addition to the index pool!');
+			}
+		}).on('error', function() {
+			console.log('Bad Request.');
+		});
+		
+		console.log('sending: ' + postData);
+		req.end(postData);	
 	}
 
 };
 
-module.exports.indexFileCreateTest();
+module.exports.indexFileSendToServerTest();
