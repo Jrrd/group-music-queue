7bee3288d9c491ce5e19f65b81932abf561145ed
Frank Hanner <luapdeux@gmail.com>
2013-11-23 07:53:19 -0800

    More enhancements to the Indexer/IndexPool. All IndexPool endpoints should be in place now.

--- tests/TestIndexer.js
+++ tests/TestIndexer.js
@@ -2,16 +2,16 @@ var Indexer = require('../lib/shared/utils/Indexer'),
 	http = require('http'),
 	fs = require('fs');
 
-var indexer = new Indexer("frank");
+var indexer = new Indexer();
 
 module.exports = { 
 	
 	indexFileCreateTest : function(){
-		indexer.createFile("./top", ".");
+		indexer.createFile("./medium", ".");
 	},	
 
 	indexFileUpdateTest : function(){
-		indexer.updateFile(".", "./song-index.json")
+		indexer.updateFile("./top", "./song-index.json")
 	},
 	
 	indexFileSendToServerTest : function() {
@@ -20,7 +20,7 @@ module.exports = {
 				host : 'localhost',
 				port : '3800',
 				method : 'POST',
-				path : '/indexfile',
+				path : '/indexfile/frank',
 				headers: {
 					'Content-Type': 'application/json',
 					'Content-Length': Buffer.byteLength(postData)				
@@ -37,7 +37,84 @@ module.exports = {
 		
 		console.log('sending: ' + postData);
 		req.end(postData);	
-	}
+	},
+	
+	indexFileRemoveFromServerTest : function() {
+		this.indexFileSendToServerTest();
+		setTimeout(function(){
+			var options = {
+				host : 'localhost',
+				port : '3800',
+				method : 'DELETE',
+				path : '/indexfile/frank',
+				headers: {}
+			};
+		
+		var req = http.request(options, function(res) {
+			if (res.statusCode === 204) {
+				console.log('Successful deletion from the index pool!');
+			}
+			process.exit(0);
+		}).on('error', function() {
+			console.log('Bad Request.');
+			process.exit(1);
+		});
+		
+		console.log('sending...');
+		req.end();	}, 5000)	
+	},
+	
+	retrieveSingleSongListFromServer : function() {
+		this.indexFileSendToServerTest();
+		setTimeout(function(){
+			var options = {
+				host : 'localhost',
+				port : '3800',
+				method : 'GET',
+				path : '/indexfile/frank',
+				headers: {}
+			};
+		
+		var req = http.request(options, function(res) {
+			res.on("data", function(chunk) {
+			    console.log("BODY: " + chunk);
+				process.exit(0)
+			  });
+		}).on('error', function() {
+			console.log('Bad Request.');
+			process.exit(1);
+		});
+		
+		console.log('sending...');
+		req.end();	}, 5000)
+		
+	},
+	
+	retrieveAllSongsFromServer : function() {
+			this.indexFileSendToServerTest();
+			setTimeout(function(){
+				var options = {
+					host : 'localhost',
+					port : '3800',
+					method : 'GET',
+					path : '/indexfile/',
+					headers: {}
+				};
+		
+			var req = http.request(options, function(res) {
+				res.on("data", function(chunk) {
+				    console.log("BODY: " + chunk);
+					process.exit(0)
+				  });
+			}).on('error', function() {
+				console.log('Bad Request.');
+				process.exit(1);
+			});
+		
+			console.log('sending...');
+			req.end();	}, 5000)
+		
+		}
 
 };
 
